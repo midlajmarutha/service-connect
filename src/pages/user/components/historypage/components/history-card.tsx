d3ef87b583b58f22cc702f2ba54abfde6c34eb99
Midlaj Marutha <midlajmarutha4@gmail.com>
2024-08-21 19:17:33 +0530

    updation

--- src/pages/user/components/historypage/components/history-card.tsx
+++ src/pages/user/components/historypage/components/history-card.tsx
@@ -18,6 +18,9 @@ const HistoryCard = ({item}:{item:ServiceHistory}) => {
         <div>
           {item.time}
         </div>
+        <div>
+          {item.service.price}
+        </div>
       </div>
     </div>
   )
