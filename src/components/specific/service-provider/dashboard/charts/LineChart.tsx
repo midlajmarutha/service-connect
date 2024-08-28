63bf0c4ce0fbc12f229c04649e63e82aae8cebc5
Midlaj Marutha <midlajmarutha4@gmail.com>
2024-08-28 16:32:27 +0530

    ui: dashboard

---
+++ src/components/specific/service-provider/dashboard/charts/LineChart.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import {
+    CartesianGrid,
+    Legend,
+    Line,
+    Tooltip,
+    XAxis,
+    YAxis,
+    LineChart as LineChartContainer,
+    ResponsiveContainer,
+} from "recharts";
+
+const data = [
+    {
+        name: "Page A",
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: "Page B",
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: "Page C",
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: "Page D",
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: "Page E",
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: "Page F",
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: "Page G",
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
+];
+
+const LineChart = () => {
+    return (
+        <div className="flex w-full p-4 bg-white rounded-md shadow-md">
+            <ResponsiveContainer width={"100%"} height={200}>
+            <LineChartContainer
+                width={730}
+                height={250}
+                data={data}
+                margin={{right:10, left:10}}
+            >
+                <YAxis axisLine={false} tickLine={false} style={{fontFamily:"poppins"}} className="text-blue-600 text-xs"/>
+                <CartesianGrid strokeDasharray="1 1" opacity={0.4} vertical={false}/>
+                <Tooltip />
+                <Line type="linear" dataKey="pv" stroke="#0000ff" />
+            </LineChartContainer>
+            </ResponsiveContainer>
+        </div>
+    );
+};
+
+export default LineChart;
--- src/pages/service-provider/dashboad/provider-dashboard.tsx
+++ src/pages/service-provider/dashboad/provider-dashboard.tsx
@@ -1,12 +1,17 @@
 import Header from '@/components/shared/header'
 import SearchBar from '@/components/shared/searchbar'
+import LineChart from '@/components/specific/service-provider/dashboard/charts/LineChart'
 import React from 'react'
+
 
 const ProviderDashboard = () => {
   return (
-    <div className='bg-slate-100 min-h-screen'>
+    <div className='bg-slate-100 min-h-screen w-full'>
       <Header/>
       <SearchBar/>
+      <div className='p-4 w-full'>
+        <LineChart/>
+      </div>
     </div>
   )
 }
