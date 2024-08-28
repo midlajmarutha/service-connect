ca14bfe46327aa2dccdf32f626fccc8f5493f134
Midlaj Marutha <midlajmarutha4@gmail.com>
2024-08-28 15:31:47 +0530

    search bar ui filter icon change

--- src/components/shared/searchbar.tsx
+++ src/components/shared/searchbar.tsx
@@ -1,4 +1,4 @@
-import { FilterIcon, SearchIcon } from 'lucide-react'
+import { FilterIcon, SearchIcon, SlidersHorizontalIcon } from 'lucide-react'
 import React, { FocusEventHandler, FormEventHandler, MouseEventHandler } from 'react'
 
 const SearchBar = ({onSearch, onValueChange, onFocus}:{onSearch?:MouseEventHandler,onValueChange?:FormEventHandler, onFocus?:FocusEventHandler}) => {
@@ -16,7 +16,7 @@ const SearchBar = ({onSearch, onValueChange, onFocus}:{onSearch?:MouseEventHandl
             onFocus={e=>{onFocus && onFocus(e)}}
             onBlur={e=>{onFocus && onFocus(e)}}/>
         <div className='h-10 w-10 flex items-center justify-center text-blue-700 bg-white hover:bg-gray-100 hover:text-gray-700 transition-all cursor-pointer rounded-r-md'>
-            <FilterIcon size={"18px"}/>
+            <SlidersHorizontalIcon size={"18px"}/>
         </div>
     </div>
   )
